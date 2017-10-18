8624cd378855052dcadcc5979091deb281c4225e
Łukasz Reszka <lukasz.res@gmail.com>
2017-10-18 16:23:42 +0200

    main.cpp checking version

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include "mainwindow.h"
 #include <QApplication>
-
+#include "versionchecker.h"
+#include <QDebug>
 int main(int argc, char *argv[])
 {
+    float version{1.3};
+    QString versionURL{"https://raw.githubusercontent.com/lukaszres/QProgramTV/master/version.txt"};
+
     QApplication a(argc, argv);
+
+    VersionChecker vChecker;
+    vChecker.setVersion(version);
+    vChecker.setFilePath(versionURL);
+    if (vChecker.isActual())
+        qDebug()<<"Twoja wersja jest aktualna.";
+    else
+        qDebug()<<"Twoja wersja jest nieaktualna";
+
     MainWindow w;
     w.show();
 
